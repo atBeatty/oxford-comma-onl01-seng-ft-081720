6aa2cc9b581aadba9acc69fbefbedc97248fde1c
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-17 21:11:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma(array)
   formatted_string = ''
-  if array.size == 1
+  if array.size = 1
     formatted_string = array[0]
   end
-  if array.size == 2
+  if array.size = 2
     formatted_string = array.join(",")
-    puts formatted_string
+
   end
 
 return formatted_string
