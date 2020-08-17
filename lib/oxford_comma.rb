f9655f58dd8a76d4d5486a5e608097239c82ffb7
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-17 21:20:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,10 +4,10 @@ def oxford_comma(array)
     formatted_string = array[0]
   end
   if array.size == 2
-    formatted_string = array.join("and ")
+    formatted_string = array.join(" and ")
   end
   if array.size > 2
-    last_element = " and " + array.last
+    last_element = "and " + array.last
     counter = 0
     while counter < array.size - 1
       formatted_string += array[counter] + ", "
